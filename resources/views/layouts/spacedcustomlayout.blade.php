639cef99cf70b29731bc3fa242dd1a71601b3770
romeoscript <romeobourne211@gmail.com>
2023-07-11 20:52:25 +0100

    feat:tidio

--- resources/views/layouts/spacedcustomlayout.blade.php
+++ resources/views/layouts/spacedcustomlayout.blade.php
@@ -106,19 +106,7 @@
     <link rel="stylesheet" href="../cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
     <link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons'>
     <link rel="stylesheet" href="style.html">
- <!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/64a64f2394cf5d49dc61d3ab/1h4kqa9n1';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
+    <script src="//code.tidio.co/hp4lim95ncjumkqcqfnlsk2vcopicghj.js" async></script>
 
     <style>
         * {
@@ -578,19 +566,7 @@ s0.parentNode.insertBefore(s1,s0);
     </script>
 
 
- <!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/64a64f2394cf5d49dc61d3ab/1h4kqa9n1';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
+    <script src="//code.tidio.co/hp4lim95ncjumkqcqfnlsk2vcopicghj.js" async></script>
     <!--End of Tawk.to Script-->
     <script src="assets/js/jquery.js"></script>
     <script src="assets/js/popper.min.js"></script>
